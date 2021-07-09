02c1a5392f8445823b1a5093d86f223753ee4048
Nigel Metheringham <nigelm@cpan.org>
2021-07-09 11:34:34 +0100

    Initial attempt at code
    No settings codes - only defaults

--- octoprint_mote_lighting_control/__init__.py
+++ octoprint_mote_lighting_control/__init__.py
@@ -1,29 +1,125 @@
+import time
+
 import octoprint.plugin
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
+# initial mote import
+try:
+    from mote import Mote
+
+    mote_type = "USB"
+
+except ImportError:
+    try:
+        import motephat as mote
+
+        mote_type = "PHAT"
+    except ImportError:
+        import sys
+
+        sys.exit(-1)
 
 
-class Mote_lighting_controlPlugin(
+# -----------------------------------------------------------------------
+class MoteLightingControlPlugin(
     octoprint.plugin.SettingsPlugin,
     octoprint.plugin.AssetPlugin,
     octoprint.plugin.TemplatePlugin,
+    octoprint.plugin.StartupPlugin,
+    octoprint.plugin.EventHandlerPlugin,
 ):
 
-    ##~~ SettingsPlugin mixin
+    # -----------------------------------------------------------------------
+    def __init__(self):
+        self.mote = None
+        self.mote_type = None
+        self.colour = "#FFFFFF"
+        self.lights_on = False
 
+    # -----------------------------------------------------------------------
+    def initialise_leds(self):
+        if self.mote is not None:
+            self._logger.warning("Attempting to initialise an already initialised mote system")
+        self.mote_type = mote_type
+        self._logger.info(f"Initialising Mote({self.mote_type}) Lighting")
+        if self.mote_type == "USB":
+            self.mote = Mote()
+            # set up 4 channels
+            for channel in range(1, 5):
+                mote.configure_channel(channel=1, num_pixels=16, gamma_correction=False)
+        else:
+            self.mote = mote
+        self.set_leds(self.lights_on, self.colour)
+
+    # -----------------------------------------------------------------------
+    def set_leds(self, lights_on: bool = True, colour: str = "#FFFFFF"):
+        hex = colour.lstrip("#")
+        red, green, blue = tuple(int(hex[i : i + 2], 16) for i in (0, 2, 4))
+        if lights_on is False or (red == 0 and green == 0 and blue == 0):
+            self._logger.info("Mote: setting LEDs off")
+            self.lights_on = False
+            mote.clear()
+        else:
+            self._logger.info(f"Mote: setting LEDs on to colour {colour}")
+            self.lights_on = True
+            self.colour = colour
+            self.mote.set_all(r=red, g=green, b=blue, brightness=1.0)
+        self.mote.show()
+
+    # -----------------------------------------------------------------------
+    def on_event(self, event, payload):
+        initial_lights_on = self.lights_on
+        initial_colour = self.colour
+        transitory = False
+        lights_on = True
+        colour = "#0000ff"  # deep blue - not used elsewhere
+        if event == "Startup":
+            colour = self._settings.get(["startup_colour"])
+        elif event == "Connected":
+            colour = self._settings.get(["connect_colour"])
+        elif event == "Disconnected":
+            colour = self._settings.get(["disconnect_colour"])
+        elif event == "Upload":
+            colour = self._settings.get(["upload_colour"])
+            transitory = True
+        elif event == "PrintStarted":
+            colour = self._settings.get(["printing_colour"])
+        elif event == "PrintFailed" or event == "Error":
+            colour = self._settings.get(["error_colour"])
+        elif event == "PrintDone":
+            colour = self._settings.get(["done_colour"])
+        elif event == "ClientOpened":
+            colour = self._settings.get(["connect_colour"])
+            transitory = True
+        elif event == "ClientClosed":
+            colour = self._settings.get(["error_colour"])
+            transitory = True
+        else:
+            # not an event we handle... just return
+            return
+
+        self._logger.info(f"Mote: Setting lighting for event {event} to colour {colour}")
+        self.set_leds(lights_on=lights_on, colour=colour)
+        if transitory:
+            time.sleep(4.0)
+            self.set_leds(lights_on=initial_lights_on, colour=initial_colour)
+
+    # -----------------------------------------------------------------------
     def get_settings_defaults(self):
-        return {
-            # put your plugin's default settings here
-        }
+        """Default settings for the plugin"""
+        return dict(
+            lights_on=False,  # lights currently off
+            current_colour="#FFFFFF",  # current - white
+            mote_type=mote_type,
+            startup_colour="#94adff",  # startup - blue
+            error_colour="#FF0000",  # error - red
+            printing_colour="#FFFFFF",  # printing - white
+            connect_colour="#96fa91",  # connect - green
+            done_colour="#ff8cc9",  # completed - pink
+            upload_colour="#ffff00",  # upload - yellow
+            disconnect_colour="#000000",  # disconnect - black/off
+        )
 
-    ##~~ AssetPlugin mixin
-
+    # -----------------------------------------------------------------------
     def get_assets(self):
         # Define your plugin's asset files to automatically include in the
         # core UI here.
@@ -33,8 +129,7 @@ class Mote_lighting_controlPlugin(
             "less": ["less/mote_lighting_control.less"],
         }
 
-    ##~~ Softwareupdate hook
-
+    # -----------------------------------------------------------------------
     def get_update_information(self):
         # Define the configuration for your plugin to use with the Software Update
         # Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
@@ -57,19 +152,13 @@ class Mote_lighting_controlPlugin(
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Mote_lighting_control Plugin"
-
-# Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
-# Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
-# compatibility flags according to what Python versions your plugin supports!
-# __plugin_pythoncompat__ = ">=2.7,<3" # only python 2
-# __plugin_pythoncompat__ = ">=3,<4" # only python 3
-# __plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
+__plugin_name__ = "Mote Lighting Control Plugin"
+__plugin_pythoncompat__ = ">=3,<4"  # only python 3
 
 
 def __plugin_load__():
     global __plugin_implementation__
-    __plugin_implementation__ = Mote_lighting_controlPlugin()
+    __plugin_implementation__ = MoteLightingControlPlugin()
 
     global __plugin_hooks__
     __plugin_hooks__ = {
