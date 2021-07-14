12842540a328865122ad5022a76839718b3cb019
Nigel Metheringham <nigelm@cpan.org>
2021-07-14 20:32:34 +0100

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ plugin_package = "octoprint_mote_lighting_control"
 plugin_name = "OctoPrint-MoteLightingControl"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.3.1"
+plugin_version = "0.3.2"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
